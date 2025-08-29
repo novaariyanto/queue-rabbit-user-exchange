a5f63279715e11031884b557aafe064525b5485f
novaariyanto <bajangsampoerna@gmail.com>
2025-08-29 09:59:23 +0700

    master

--- sendmessage-queue.php
+++ sendmessage-queue.php
@@ -31,19 +31,27 @@ $headers = [
   "Accept: application/json"
 ];
 
-function postJson($url, $data, $headers) {
-  $ch = curl_init($url);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-  curl_setopt($ch, CURLOPT_POST, true);
-  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-  curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
-  $res = curl_exec($ch);
-  if (curl_errno($ch)) { echo "cURL Error: " . curl_error($ch); }
-  else { echo "Response: " . $res . PHP_EOL; }
-  curl_close($ch);
+function postJson($url, $data, $headers,$i=1) {
+    $data["userId"] = "user".$i;
+   
+    if($i % 2 == 1){
+      $data["delaySeconds"] =  $data["delaySeconds"]+1;
+    }
+    $data["payload"]["message"] = "message ".$i." user".$i." delay ".$data["delaySeconds"];
+  
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+    $res = curl_exec($ch);
+    if (curl_errno($ch)) { echo "cURL Error: " . curl_error($ch); }
+    else { echo "Response: " . $res . PHP_EOL; }
+    curl_close($ch);
 }
 
-for ($i = 0; $i < 10; $i++) {
-  postJson($api, $body1, $headers);
-  postJson($api, $body2, $headers);
+for ($i = 0; $i < 1000; $i++) {
+  for ($j = 0; $j < 1; $j++) {
+    postJson($api, $body1, $headers,$i);
+  }
 }
